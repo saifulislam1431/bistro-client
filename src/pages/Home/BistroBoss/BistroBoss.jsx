924e754fec982deee9a779801e4bef8642691405
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-23 20:51:41 +0600

    Bistro banner added

---
+++ src/pages/Home/BistroBoss/BistroBoss.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Bistro.css";
+const BistroBoss = () => {
+    return (
+        <section className='my-10'>
+<div className='bgImg bg-fixed py-10 lg:mx-10 px-10'>
+    <div className='bg-white lg:w-2/3 shadow-2xl shadow-white px-14 py-12 mx-auto text-center'>
+<h1 className='brandTitle text-2xl font-semibold my-4'>Bistro Boss</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum adipisci soluta distinctio! In sapiente enim ratione unde architecto, dolores deserunt molestias voluptas a, laborum necessitatibus quo. Dolore exercitationem quaerat ut tenetur similique provident quasi rerum saepe.</p>
+    </div>
+</div>
+        </section>
+    );
+};
+
+export default BistroBoss;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import OrderSlider from '../OrderSlider/OrderSlider';
+import BistroBoss from '../BistroBoss/BistroBoss';
 
 const Home = () => {
     return (
         <section>
             <Banner />
             <OrderSlider />
+            <BistroBoss />
         </section>
     );
 };
